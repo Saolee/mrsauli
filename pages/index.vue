6e2472b9b5924baaca532d262193b298e7570fe0
Sauli Warmenhoven <s.p.warmenhoven@hr.nl>
2020-03-03 14:28:43 +0100

    remove grid stuff, basic mrmd boilerplate

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,24 @@
 <template>
-  <div class="mx-auto text-center grid grid-container">
-    <header>
-      <h1 class="font-display font-bold mt-12 text-3xl md:text-6xl text-gray-100">
-        <span class="relative z-0">GR</span>
-        <span class="relative z-10 text-gray-800">//</span>
-        <span class="relative z-20">ID</span>
-        <span class="relative z-10 text-gray-800">.</span>
+  <div
+    class="mx-auto text-center max-w-screen-lg flex flex-col items-center tex h-screen justify-center"
+  >
+    <header class>
+      <h1 class="font-display font-bold text-gray-100">
+        <span class="relative z-0 -mr-4">MR</span>
+        <span class="relative z-10 text-gray-800 -mx-4">//</span>
+        <span class="relative z-20 -mx-6">MD</span>
+        <span class="relative z-10 -ml-4 text-gray-800">.</span>
       </h1>
 
       <h2
-        class="font-display lowercase italic font-medium opacity-25 text-gray-100 text-xl mb-8"
-      >Grid stuff</h2>
+        class="font-display lowercase italic font-medium opacity-50 text-gray-100 text-xl"
+      >For beautiful websites</h2>
     </header>
-    <main>
-      <div class="card">
-        <h2 class="title font-display font-bold text-xl">1 alonger title, spanning multiple lines</h2>
-        <div class="excerpt text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-        <div class="category">Course</div>
-      </div>
-      <div class="card">
-        <h2 class="title font-display font-bold text-xl">2 title</h2>
-        <div class="excerpt text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-        <div class="category">Display</div>
-      </div>
-      <div class="card">
-        <h2 class="title font-display font-bold text-xl">3 title</h2>
-        <div class="excerpt text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-        <div class="category">Event</div>
-      </div>
-      <div class="card">
-        <h2 class="title font-display font-bold text-xl">4 title</h2>
-        <div class="excerpt text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-        <div class="category">Class</div>
-      </div>
-      <div class="card">
-        <h2 class="title font-display font-bold text-xl">5 title</h2>
-        <div class="excerpt text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit...</div>
-        <div class="category">Course</div>
-      </div>
+    <main class="mt-8">
+      <a
+        href="mailto:mr.sauli@gmail.com"
+        class="bg-gray-400 text-gray-900 px-4 py-3 hover:bg-gray-300 focus:bg-gray-300 rounded-lg uppercase"
+      >contact us</a>
     </main>
   </div>
 </template>
@@ -45,38 +26,7 @@
 export default {};
 </script>
 <style lang="scss">
-.grid-container {
-  @media screen and(min-width:700px) {
-    grid-template-columns: minmax(280px, 1fr) 3fr;
-  }
-}
-main {
-  display: grid;
-  grid-gap: 10px;
-  grid-auto-rows: 220px;
-  @media screen and (min-width: 700px) {
-    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
-  }
-}
-.card {
-  background: white;
-  border-top: 1px solid black;
-  border-bottom: 1px solid black;
-  display: grid;
-  grid-template-rows: subgrid;
-  padding: 10px;
-}
-.title {
-  text-align: left;
-  max-height: 3em;
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-.excerpt {
-  align-self: center;
-}
-.category {
-  justify-self: left;
-  align-self: end;
+header h1 {
+  font-size: 5rem;
 }
 </style>
